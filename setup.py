fd21510838cd20340eecc39d50401bd1f723ff97
Sergey Panfilov <sergray@gmail.com>
2020-11-07 08:50:04 +0100

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
-  long_description = f.read()
+    long_description = f.read()
 
 setup(
-  name="eiqlib",
-  version="1.0.0",
-  author="Sebastiaan Groot",
-  author_email="sebastiaang@kpn-cert.nl",
-  description="A python3 library for interacting with EclecticIQ",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/KPN-CISO/eiqlib",
-  packages=find_packages(),
-  classifiers=(
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-  ),
+    name="eiqlib",
+    version="1.0.0",
+    author="Sebastiaan Groot",
+    author_email="sebastiaang@kpn-cert.nl",
+    description="A python3 library for interacting with EclecticIQ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KPN-CISO/eiqlib",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
-
